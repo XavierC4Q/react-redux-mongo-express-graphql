73b3769da0af957e8a996ad4fa744d7c882f6ae8
Xavier <xaviermunroe@ac.c4q.nyc>
2018-08-29 18:00:15 -0400

    set up apollo server with a redux provider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {ApolloClient} from 'apollo-client';
+import {ApolloProvider} from 'react-apollo';
+import {HttpLink} from 'apollo-link-http';
+import {InMemoryCache} from 'apollo-cache-inmemory'
+import {createStore, applyMiddleware} from 'redux'
+import allReducers from './reducers/rootreducer'
+import thunk from 'redux-thunk'
+import {Provider as ReduxProvider} from 'react-redux'
+import {BrowserRouter} from 'react-router-dom'
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+  link: new HttpLink({uri: `localhost:8080/api`}),
+  cache: new InMemoryCache()
+})
+
+const storeConfig = (initialState) => {
+  return createStore(allReducers, initialState, applyMiddleware(thunk))
+}
+
+const store = storeConfig()
+
+ReactDOM.render(<ApolloProvider client={client}>
+  <ReduxProvider store={store}>
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
+  </ReduxProvider>
+</ApolloProvider>, document.getElementById('root'));
 registerServiceWorker();
